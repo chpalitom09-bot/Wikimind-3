e16771355d70a10f73384645644844bee5a3cea5
Tom09s <chpalitom09@gmail.com>
2026-03-14 10:59:46 +0100

    Delete wikimind_game directory

--- wikimind_game/CSGO-sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME = "wikimind-csgo-v1";
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(["./index.html", "./style.css"])));
-});
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
